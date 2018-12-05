d26b93b93debdfb0e0d154c14afbef506151bf8e
KushalShah1 <kushalshah@flash.net>
2018-12-05 17:06:03 -0600

    Half completed iteration 2

--- Main.asm
+++ Main.asm
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ; Main.asm
 ; Name: Ethan Shen, Kushal Shah
 ; UTEid: 
@@ -22,13 +21,40 @@ STI R0,KBSR
 
 
 ; start of actual program
-START	
+
 	AND R0,R0,0
 	STI R0,BUFFER
-	LDI R0,BUFFER
+START	LDI R0,BUFFER
 	BRz START
 
+	AND R1,R1,0
+	STI R1,BUFFER
+	
+
+	LD R1, A
+	ADD R1, R1, R0
+	BRz VALID
+	
+	LD R1, C
+	ADD R1, R1, R0
+	BRz VALID
+
+	LD R1, G
+	ADD R1, R1, R0
+	BRz VALID
+
+	LD R1, U
+	ADD R1, R1, R0
+	BRnp VALID
+	
+VALID
 	TRAP X25
+
+
+A	.FILL #-65
+C	.FILL #-67
+G	.FILL #-71
+U	.FILL #-85
 
 BUFFER	.FILL X4600
 HEADER 	.FILL X4000
@@ -37,43 +63,3 @@ KBSR 	.FILL XFE00
 IVT 	.FILL X0180
 INRPT	.FILL X2600
 		.END
-=======
-; Main.asm
-; Name:
-; UTEid: 
-; Continuously reads from x4600 making sure its not reading duplicate
-; symbols. Processes the symbol based on the program description
-; of mRNA processing.
-               .ORIG x4000
-; initialize the stack pointer
-LD R6,HEADER
-
- 
-; set up the keyboard interrupt vector table entry
-
-LD R0,INRPT
-STI R0,IVT
-
-; enable keyboard interrupts
-
-LD R0,KBIMASK
-STI R0,KBSR
-
-
-; start of actual program
-START	
-	AND R0,R0,0
-	STI R0,BUFFER
-	LDI R0,BUFFER
-	BRz START
-
-	TRAP X25
-
-BUFFER	.FILL X4600
-HEADER 	.FILL X4000
-KBIMASK	.FILL X4000
-KBSR 	.FILL XFE00
-IVT 	.FILL X0180
-INRPT	.FILL X2600
-		.END
->>>>>>> 0f82b434907ba6b72b58d0676f5189782b80e94d
